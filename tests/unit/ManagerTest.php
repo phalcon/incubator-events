abff94c2be18cdc00f195c4ba82b6028baa7d8ab
Aziz <bemyslavedarlin@gmail.com>
2020-06-27 01:00:51 +0300

    Fix tests

--- tests/unit/ManagerTest.php
+++ tests/unit/ManagerTest.php
@@ -1,30 +1,23 @@
 <?php
 
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types = 1);
 
 namespace Phalcon\Incubator\Events\Tests\Unit;
 
+use Codeception\Test\Unit;
 use Phalcon\Events\ManagerInterface;
 use Phalcon\Incubator\Events\Manager;
 
-/**
- * \Phalcon\Incubator\Events\Tests\Unit\ManagerTest
- * Tests for Phalcon\Incubator\Events\Manager component
- *
- * @copyright (c) 2011-2016 Phalcon Team
- * @author        Aziz Muzafarov <bemyslavedarlin@gmail.com>
- * @link          http://phalconphp.com/
- * @package       Phalcon\Incubator\Events\Tests\Unit
- * @group         Events
- *
- * The contents of this file are subject to the New BSD License that is
- * bundled with this package in the file docs/LICENSE.txt
- *
- * If you did not receive a copy of the license and are unable to obtain it
- * through the world-wide-web, please send an email to license@phalconphp.com
- * so that we can send you a copy immediately.
- */
-class ManagerTest extends UnitTester
+class ManagerTest extends Unit
 {
     public function testImplementation(): void
     {
